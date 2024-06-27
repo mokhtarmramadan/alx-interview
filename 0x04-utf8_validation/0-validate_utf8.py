98b6a2f325aa520b52b7f74496e8e48db8458f2d
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-06-27 22:07:54 +0300

    06-27 22:07

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
-binary_rep_for_n = []
-
-
-def DecimalToBinary(num):
-    ''' Converts a decimal number to its bianry representation '''
-    if num >= 1:
-        DecimalToBinary(num // 2)
-    binary_rep_for_n.append(num % 2)
-    return (num % 2)
-
-
 def validUTF8(data):
-    ''' determines if a given data set represents a valid UTF-8 encoding '''
-    binary_numbers = []
+    """Check if data is valid UTF-8 encoding."""
+    n_bytes = 0
+
     for num in data:
-        del binary_rep_for_n[:]
-        DecimalToBinary(num)
-        if len(binary_rep_for_n) > 8:
-            return False
-    return True
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            if bin_rep[0] == '0':
+                continue
+            elif bin_rep[:3] == '110':
+                n_bytes = 1
+            elif bin_rep[:4] == '1110':
+                n_bytes = 2
+            elif bin_rep[:5] == '11110':
+                n_bytes = 3
+            else:
+                return False
+        else:
+            if not bin_rep.startswith('10'):
+                return False
+        n_bytes -= 1
+
+    return n_bytes == 0
