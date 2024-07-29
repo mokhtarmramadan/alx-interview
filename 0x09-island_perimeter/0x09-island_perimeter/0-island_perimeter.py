26ae1b4b5f16fc4d05c0074201258328f3ea430e
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-07-29 16:46:04 +0300

    Added island_perimeter function

---
+++ 0x09-island_perimeter/0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    ''' returns the perimeter of the island described in grid '''
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                try:
+                    if j > 0:
+                        if grid[i][j - 1] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if i > 0:
+                        if grid[i - 1][j] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                try:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+    return perimeter
