1bd3f5596f9241f4995deaad515db278e4bb15c8
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-08-06 18:35:57 +0300

    hot fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,7 @@ def isWinner(x, nums):
         is_prime = [True] * (n + 1)
         p = 2
         while (p * p <= n):
-            if (is_prime[p] == True):
+            if is_prime[p] is True:
                 for i in range(p * p, n + 1, p):
                     is_prime[i] = False
             p += 1
@@ -17,30 +17,30 @@ def isWinner(x, nums):
             if is_prime[p]:
                 primes.append(p)
         return primes
-    
+
     def count_primes(n, primes):
         count = 0
         for prime in primes:
             if prime <= n:
                 count += 1
         return count
-    
+
     if x == 0:
         return None
-    
+
     max_n = max(nums)
     primes = sieve(max_n)
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         prime_count = count_primes(n, primes)
         if prime_count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
